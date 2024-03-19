8bd9ce1b72552340cd16495fb835c48a0fdd61ed
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-19 07:23:40 -0600

    feat: touchable image

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableHighlight, SafeAreaView } from 'react-native';
 
 export default function App() {
+  const handleTouch = () => { console.log('Image touched!') };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello React Native!</Text>
+      <TouchableHighlight onPress={handleTouch}>
+        <Image
+          source={{
+            width: 200,
+            height: 200,
+            uri: 'https://picsum.photos/200'
+          }}
+        />
+      </TouchableHighlight>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +23,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
