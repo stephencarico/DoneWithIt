6067015307d70594081832a28faae9469e9d9cac
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-19 14:27:12 -0600

    feat: useDeviceOrientation

--- App.js
+++ App.js
@@ -1,20 +1,16 @@
-import { Image, StyleSheet, Text, TouchableHighlight, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, Platform, View } from 'react-native';
+import { useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
-  const handleTouch = () => { console.log('Image touched!') };
+  const orientation = useDeviceOrientation();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <TouchableHighlight onPress={handleTouch}>
-        <Image
-          source={{
-            width: 200,
-            height: 200,
-            uri: 'https://picsum.photos/200'
-          }}
-        />
-      </TouchableHighlight>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '100%',
+        height: orientation === 'landscape' ? '100%' : '30%'
+      }}></View>
     </SafeAreaView>
   );
 }
@@ -23,7 +19,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
