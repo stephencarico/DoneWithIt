aca38e9ac3932af6be308758c19736f30789f813
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-20 07:44:59 -0600

    feat: ViewImageScreen course approach

---
+++ app/screens/MyViewImageScreen.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Image, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+
+function OtherScreen(props) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        barStyle={'default'}
+      />
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingHorizontal: 40
+      }}>
+        <View style={{
+          height: 50,
+          width: 50,
+          backgroundColor: '#fc5c65'
+        }} />
+        <View style={{
+          height: 50,
+          width: 50,
+          backgroundColor: '#4ecdc4'
+        }} />
+      </View>
+      <Image source={require('../assets/chair.jpg')} style={styles.image} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+  image: {
+    height: '70%',
+    width: '100%',
+    top: 50,
+  }
+})
+
+export default OtherScreen;
--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,43 +1,45 @@
 import React from 'react';
-import { Image, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 
-function OtherScreen(props) {
+function ViewImageScreen(props) {
   return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar
-        barStyle={'default'}
+    <View style={styles.container}>
+      <View style={styles.closeIcon}></View>
+      <View style={styles.deleteIcon}></View>
+      <Image
+        style={styles.image}
+        resizeMode='contain'
+        source={require('../assets/chair.jpg')}
       />
-      <View style={{
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        paddingHorizontal: 40
-      }}>
-        <View style={{
-          height: 50,
-          width: 50,
-          backgroundColor: '#fc5c65'
-        }} />
-        <View style={{
-          height: 50,
-          width: 50,
-          backgroundColor: '#4ecdc4'
-        }} />
-      </View>
-      <Image source={require('../assets/chair.jpg')} style={styles.image} />
-    </SafeAreaView>
+    </View>
   );
 }
 
-export default OtherScreen;
-
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#fc5c65',
+    position: 'absolute',
+    top: 40,
+    left: 30
+  },
   container: {
     flex: 1,
     backgroundColor: 'black',
   },
+  deleteIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#4ecdc4',
+    position: 'absolute',
+    top: 40,
+    right: 30
+  },
   image: {
-    height: '70%',
-    width: '100%',
-    top: 50,
+    height: '100%',
+    width: '100%'
   }
 })
+
+export default ViewImageScreen;
