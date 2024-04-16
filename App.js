115e0203d019b386b426366c3016cf9d0dbad3ec
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-15 22:57:51 -0600

    feat: expand listings

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import MyAccountScreen from './app/screens/MyAccountScreen';
+import ListingsScreen from './app/screens/ListingsScreen';
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <MyAccountScreen />
+      <ListingsScreen />
     </GestureHandlerRootView>
   );
 }
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,16 +1,35 @@
-import { View, StyleSheet, ScrollView } from 'react-native'
+import { View, StyleSheet, ScrollView, FlatList } from 'react-native'
 import React from 'react'
 
 import Card from '../components/Card';
 
 function ListingsScreen() {
+  const listings = [
+    {
+      title: 'Red jacket for sale!',
+      price: '$100',
+      image: require('../assets/jacket.jpg')
+    },
+    {
+      title: 'Couch in great condition',
+      price: '$100',
+      image: require('../assets/couch.jpg')
+    },
+  ];
+
   return (
     <View style={styles.container}>
       <ScrollView style={styles.listArea}>
-        <Card
-          title="Red jacket for sale!"
-          subTitle="$100"
-          image={require('../assets/jacket.jpg')}
+        <FlatList
+          data={listings}
+          keyExtractor={listing => listing.title}
+          renderItem={({ item }) => 
+            <Card
+              title={item.title}
+              subTitle={item.price}
+              image={item.image}
+            />
+          }
         />
       </ScrollView>
     </View>
