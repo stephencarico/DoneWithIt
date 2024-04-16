b8d4bce2efef8535056471744fac04d4e53529d4
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-16 07:32:14 -0600

    refactor: ListingsScreen

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,10 +1,12 @@
-import { SafeAreaView, StyleSheet } from 'react-native'
+import { SafeAreaView, StyleSheet, View } from 'react-native'
 import Constants from 'expo-constants';
 
 function Screen({ children, style }) {
   return (
-    <SafeAreaView style={[styles.screen, style]}>
-      {children}
+    <SafeAreaView style={styles.screen}>
+      <View style={[style, { flex: 1 }]}>
+        {children}
+      </View>
     </SafeAreaView>
   )
 }
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,49 +1,48 @@
-import { View, StyleSheet, ScrollView, FlatList } from 'react-native'
+import { StyleSheet, FlatList } from 'react-native'
 import React from 'react'
 
 import Card from '../components/Card';
+import Screen from '../components/Screen';
+import colors from '../config/colors';
+
+const listings = [
+  {
+    id: 1,
+    title: 'Red jacket for sale!',
+    price: '100',
+    image: require('../assets/jacket.jpg')
+  },
+  {
+    id: 2,
+    title: 'Couch in great condition',
+    price: '100',
+    image: require('../assets/couch.jpg')
+  },
+];
 
 function ListingsScreen() {
-  const listings = [
-    {
-      title: 'Red jacket for sale!',
-      price: '$100',
-      image: require('../assets/jacket.jpg')
-    },
-    {
-      title: 'Couch in great condition',
-      price: '$100',
-      image: require('../assets/couch.jpg')
-    },
-  ];
-
   return (
-    <View style={styles.container}>
-      <ScrollView style={styles.listArea}>
-        <FlatList
-          data={listings}
-          keyExtractor={listing => listing.title}
-          renderItem={({ item }) => 
-            <Card
-              title={item.title}
-              subTitle={item.price}
-              image={item.image}
-            />
-          }
-        />
-      </ScrollView>
-    </View>
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={listing => listing.id.toString()}
+        renderItem={({ item }) => 
+          <Card
+            title={item.title}
+            subTitle={'$' + item.price}
+            image={item.image}
+          />
+        }
+      />
+    </Screen>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f8f4f4',
-  },
-  listArea: {
+  screen: {
+    backgroundColor: colors.light,
     padding: 20,
-    paddingTop: 100
-  }
+  },
 })
 
 export default ListingsScreen;
