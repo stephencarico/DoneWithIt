cda068bf3b8a1b9e651890e45c00946555b6687f
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-17 15:23:18 -0600

    feat: Modal in AppPicker

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,17 +1,28 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, Modal, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import AppText from './AppText'
+import Screen from './Screen'
 import defaultStyles from '../config/styles'
 
 const AppPicker = ({ icon, placeholder, ...otherProps }) => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} /> }
-      <AppText style={styles.text}>{placeholder}</AppText>
-      {icon && <MaterialCommunityIcons name='chevron-down' size={20} color={defaultStyles.colors.medium} /> }
-    </View>
+    <>    
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && <MaterialCommunityIcons name={icon} size={20} color={defaultStyles.colors.medium} style={styles.icon} /> }
+          <AppText style={styles.text}>{placeholder}</AppText>
+          {icon && <MaterialCommunityIcons name='chevron-down' size={20} color={defaultStyles.colors.medium} /> }
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType='slide'>
+        <Screen>
+          <Button title='Close' onPress={() => setModalVisible(false)}/>
+        </Screen>
+      </Modal>
+    </>
   )
 }
 
