8b4d7ef30bf2146cb7f193692aaecc9dc33dc896
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-18 06:14:31 -0600

    feat: AppFormField

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -7,6 +7,7 @@ import AppButton from '../components/AppButton'
 import AppTextInput from '../components/AppTextInput'
 import ErrorMessage from '../components/ErrorMessage'
 import Screen from '../components/Screen'
+import AppFormField from '../components/AppFormField'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -23,30 +24,26 @@ const LoginScreen = () => {
         onSubmit={values => console.log(values)}
         validationSchema={validationSchema}
       >
-        { ({ handleChange, handleSubmit, errors, setFieldTouched, touched }) => (
+        { ({ handleSubmit }) => (
           <>
-            <AppTextInput
+            <AppFormField
               autoCapitalize='none'
               autoCorrect={false}
               icon='email'
               keyboardType='email-address'
-              onBlur={() => setFieldTouched('email')}
-              onChangeText={handleChange('email')}
+              name='email'
               placeholder='Email'
               textContentType='emailAddress'
             />
-            <ErrorMessage error={errors.email} visible={touched.email} />
-            <AppTextInput
+            <AppFormField
               autoCapitalize='none'
               autoCorrect={false}
               icon='lock'
-              onBlur={() => setFieldTouched('password')}
-              onChangeText={handleChange('password')}
+              name='password'
               placeholder='password'
               secureTextEntry
               textContentType='password'
             />
-            <ErrorMessage error={errors.password} visible={touched.password} />
             <AppButton title='Login' onPress={handleSubmit}/>
           </>
         ) }
