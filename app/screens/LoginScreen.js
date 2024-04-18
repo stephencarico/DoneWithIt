12f7b0349cc3d1d3d3fdee0a053bc46ab5b626c5
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-17 19:18:58 -0600

    feat: ErrorMessage

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -3,10 +3,10 @@ import React from 'react'
 import { Formik } from 'formik'
 import * as Yup from 'yup'
 
-import Screen from '../components/Screen'
 import AppButton from '../components/AppButton'
-import AppText from '../components/AppText'
 import AppTextInput from '../components/AppTextInput'
+import ErrorMessage from '../components/ErrorMessage'
+import Screen from '../components/Screen'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -34,7 +34,7 @@ const LoginScreen = () => {
               placeholder='Email'
               textContentType='emailAddress'
             />
-            <AppText style={{ color: 'red' }}>{ errors.email }</AppText>
+            <ErrorMessage error={errors.email} />
             <AppTextInput
               autoCapitalize='none'
               autoCorrect={false}
@@ -44,7 +44,7 @@ const LoginScreen = () => {
               secureTextEntry
               textContentType='password'
             />
-            <AppText style={{ color: 'red' }}>{ errors.password }</AppText>
+            <ErrorMessage error={errors.password} />
             <AppButton title='Login' onPress={handleSubmit}/>
           </>
         ) }
