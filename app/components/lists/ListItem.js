02ad9a21b8496ea0da4ec18b358a2a600b3ec82c
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-19 06:21:49 -0600

    feat: AppText numberOfLines

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -17,8 +17,8 @@ function ListItem({ image, IconComponent, onPress, showChevrons, title, renderRi
           {IconComponent}
           { image && <Image source={image} style={styles.image} /> }
           <View style={styles.detailsContainer}>
-            <AppText style={styles.title}>{title}</AppText>
-            { subTitle && <AppText style={styles.subTitle}>{subTitle}</AppText> }
+            <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
+            { subTitle && <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText> }
           </View>
           { showChevrons && <MaterialCommunityIcons name='chevron-right' size={25} style={styles.chevronIcon} /> }
         </View>
