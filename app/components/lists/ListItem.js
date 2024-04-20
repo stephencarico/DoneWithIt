af17d1719c75439260359f1826f05075c4b958f0
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-20 08:49:52 -0600

    refactor: ListItem

--- app/components/lists/ListItem.js
+++ app/components/lists/ListItem.js
@@ -1,9 +1,9 @@
 import { Image, StyleSheet, TouchableHighlight, View } from 'react-native'
 import React from 'react'
-import AppText from '../AppText'
 import Swipeable from 'react-native-gesture-handler/Swipeable';
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
+import AppText from '../AppText'
 import colors from '../../config/colors'
 
 function ListItem({ image, IconComponent, onPress, showChevrons, title, renderRightActions, subTitle }) {
@@ -20,7 +20,7 @@ function ListItem({ image, IconComponent, onPress, showChevrons, title, renderRi
             <AppText style={styles.title} numberOfLines={1}>{title}</AppText>
             { subTitle && <AppText style={styles.subTitle} numberOfLines={2}>{subTitle}</AppText> }
           </View>
-          { showChevrons && <MaterialCommunityIcons name='chevron-right' size={25} style={styles.chevronIcon} /> }
+          { showChevrons && <MaterialCommunityIcons name='chevron-right' size={25} color={colors.medium} /> }
         </View>
       </TouchableHighlight>
     </Swipeable>
@@ -31,11 +31,8 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     padding: 15,
-    backgroundColor: colors.white
-  },
-  chevronIcon: {
-    color: colors.medium,
-    alignSelf: 'center'
+    backgroundColor: colors.white,
+    alignItems: 'center'
   },
   image: {
     height: 50,
