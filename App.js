063e6e22ce03afa7ae9cc9d5c2eaa0aefbf2fb02
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-21 21:52:00 -0600

    feat: ImagePicker

--- App.js
+++ App.js
@@ -1,10 +1,37 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import Screen from './app/components/Screen';
+import { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
+import { Image, Button } from 'react-native';
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert('You need to enable permission to access the library')
+  }
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled)
+        setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log('Error reading an image', error);
+    }
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingEditScreen />
+      <Screen>
+        <Button title='Select Image' onPress={selectImage} />
+        <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      </Screen>
     </GestureHandlerRootView>
   );
 }
