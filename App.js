44945663b10fbbeeaae5f564a85c42765c17e068
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-25 07:20:44 -0600

    feat: install react-navigation packages

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import ListingEditScreen from './app/screens/ListingEditScreen';
+import { Text } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Screen from './app/components/Screen';
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+)
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+)
+
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="Tweet Details" component={TweetDetails} />
+  </Stack.Navigator>
+)
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <ListingEditScreen />
+      <NavigationContainer>
+        <StackNavigator />
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
