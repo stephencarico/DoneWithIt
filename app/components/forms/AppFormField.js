1a9e69b75b5832888b5bcbeafe81da8a166404e1
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-05-05 14:49:24 -0600

    refactor: setFieldValue

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -5,13 +5,13 @@ import AppTextInput from '../AppTextInput'
 import ErrorMessage from './ErrorMessage'
 
 const AppFormField = ({ name, width, ...otherProps }) => {
-  const { values, setFieldTouched, handleChange, errors, touched } = useFormikContext();
+  const { values, setFieldTouched, setFieldValue, errors, touched } = useFormikContext();
 
   return (
     <>
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
-        onChangeText={handleChange(name)}
+        onChangeText={text => setFieldValue(name, text)}
         value={values[name]}
         width={width}
         {...otherProps}
