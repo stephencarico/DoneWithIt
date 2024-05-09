7fac52caec5965fb1e12d474afcc2784b3f1127e
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-05-08 19:30:56 -0600

    feat: SplashScreen

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import jwtDecode from 'jwt-decode';
+import * as SplashScreen from 'expo-splash-screen';
 
 import AppNavigator from './app/navigation/AppNavigator';
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import OfflineNotice from './app/components/OfflineNotice';
-import { useEffect, useState } from 'react';
+import { useEffect, useCallback, useState } from 'react';
 import AuthContext from './app/auth/context';
 import authStorage from './app/auth/storage';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [appIsReady, setAppIsReady] = useState(false);
 
   const restoreToken = async () => {
     const token = await authStorage.getToken();
@@ -20,12 +22,30 @@ export default function App() {
   }
 
   useEffect(() => {
-    restoreToken();
+    async function prepare() {
+      try {
+        await restoreToken();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) return null;
 
   return (
     <AuthContext.Provider value={{ user, setUser }}>
-      <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureHandlerRootView style={{ flex: 1 }} onLayout={onLayoutRootView}>
         <OfflineNotice />
         <NavigationContainer theme={navigationTheme}>
           {user ? <AppNavigator /> : <AuthNavigator />}
