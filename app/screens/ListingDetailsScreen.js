f678256a42075deb5d16607edca87fbbbf508de7
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-05-10 07:25:47 -0600

    feat: add Contact Seller UI

--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,33 +1,56 @@
-import { StyleSheet, View } from 'react-native'
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from 'react-native'
 import React from 'react'
 import colors from '../config/colors'
 import { Image } from 'react-native-expo-image-cache'
+import * as Yup from 'yup'
 
 import AppText from '../components/AppText'
 import ListItem from '../components/lists/ListItem'
+import { AppForm, AppFormField, SubmitButton } from '../components/forms'
+
+const validationSchema = Yup.object({
+  message: Yup.string().required().min(1).label('Message'),
+})
 
 function ListingDetailsScreen({ route }) {
   const listing = route.params;
 
   return (
-    <View>
-      <Image
-        style={styles.image}
-        preview={{ uri: listing.images[0].thumbnail }}
-        uri={listing.images[0].url}
-      />
-      <View style={styles.detailsContainer}>
-        <AppText style={styles.title}>{listing.title}</AppText>
-        <AppText style={styles.price}>${listing.price}</AppText>
-        <View style={styles.userContainer}>
-          <ListItem
-            image={require('../assets/mosh.jpg')}
-            title="Mosh Hamedani"
-            subTitle="5 Listings"
-          />
+    <KeyboardAvoidingView
+      behavior='position'
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 100}
+    >
+      <View>
+        <Image
+          style={styles.image}
+          preview={{ uri: listing.images[0].thumbnail }}
+          uri={listing.images[0].url}
+        />
+        <View style={styles.detailsContainer}>
+          <AppText style={styles.title}>{listing.title}</AppText>
+          <AppText style={styles.price}>${listing.price}</AppText>
+          <View style={styles.userContainer}>
+            <ListItem
+              image={require('../assets/mosh.jpg')}
+              title="Mosh Hamedani"
+              subTitle="5 Listings"
+            />
+          </View>
+          <AppForm
+            initialValues={{ message: '' }}
+            onSubmit={(message) => console.log(message)}
+            validationSchema={validationSchema}
+          >
+            <AppFormField
+              maxLength={50}
+              name='message'
+              placeholder='Message...'
+            />
+            <SubmitButton title="Contact Seller" />
+          </AppForm>
         </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
